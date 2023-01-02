56b52c5cb089de6a913c1838be470ce5d68f6463
dashluu <dashluu9121997@gmail.com>
2023-01-02 10:28:19 -0800

    Used the newly added IDNode and ExpressionNode and introduced type checking for the left-hand side ID and the right-hand side expression during assignment operation.

--- src/ParserSpace/StatementParser.java
+++ src/ParserSpace/StatementParser.java
@@ -2,8 +2,11 @@ package ParserSpace;
 
 import Exceptions.SyntaxError;
 import LexerSpace.Lexer;
+import Symbols.IDInfo;
 import Symbols.SymbolTable;
 import Symbols.TypeInfo;
+import Types.TypeConversion;
+import Types.TypeTable;
 import Utilities.Block;
 import Utilities.Token;
 import Utilities.TokenType;
@@ -21,15 +24,25 @@ public class StatementParser {
         this.lexer = exprParser.getLexer();
     }
 
-    private boolean isIDValid(String idStr) {
-        char c = idStr.charAt(0);
+    public Lexer getLexer() {
+        return lexer;
+    }
+
+    /**
+     * Determines if the ID name has valid characters, namely alphanumeric and underscores.
+     *
+     * @param idName name of the ID.
+     * @return true if the ID name is valid and false otherwise.
+     */
+    private boolean isIDNameValid(String idName) {
+        char c = idName.charAt(0);
         // Check if the first character is a letter or '_'
         if (!Character.isAlphabetic(c) && c != '_') {
             return false;
         }
         // Next, check if the other characters are alphanumeric or '_'
-        for (int i = 1; i < idStr.length(); ++i) {
-            c = idStr.charAt(i);
+        for (int i = 1; i < idName.length(); ++i) {
+            c = idName.charAt(i);
             if (!Character.isAlphabetic(c) && !Character.isDigit(c) && c != '_') {
                 return false;
             }
@@ -37,6 +50,15 @@ public class StatementParser {
         return true;
     }
 
+    /**
+     * Attempts to parse an ID declaration in the form of "var ID: type =".
+     *
+     * @param scope scope in which the new ID is declared.
+     * @return an AST node that is the root of the assignment statement
+     * or null if the statement is not an definition.
+     * @throws SyntaxError if there is a syntax error.
+     * @throws IOException if there is an IO exception.
+     */
     private Node parseIDDeclaration(Block scope) throws SyntaxError, IOException {
         /*
         var ID: type = ...;
@@ -46,6 +68,7 @@ public class StatementParser {
             return null;
         }
 
+        String currTokenStr = currToken.getValue();
         TokenType currTokenType = currToken.getType();
         // Check if the first token is an ID declaration keyword
         if (currTokenType != TokenType.MUTABLE_ID_DECL) {
@@ -56,34 +79,37 @@ public class StatementParser {
         currToken = lexer.getNextToken();
         // Check if there is an ID name
         if (currToken == null) {
-            throw new SyntaxError("Missing a variable name after the declaration keyword", lexer.getCurrentLine());
-        }
-
-        SymbolTable symbolTable = SymbolTable.getInstance();
-        String currTokenStr = currToken.getValue();
-        // Check if the ID is valid
-        if (symbolTable.isID(currTokenStr, scope)) {
-            throw new SyntaxError("Cannot redeclare an existing variable", lexer.getCurrentLine());
-        } else if (symbolTable.isKeyword(currTokenStr) || symbolTable.isType(currTokenStr)) {
-            throw new SyntaxError("Cannot use a reserved keyword for a variable name", lexer.getCurrentLine());
-        } else if (symbolTable.isOperator(currTokenStr)) {
-            throw new SyntaxError("Cannot use an operator as a variable name", lexer.getCurrentLine());
-        } else if (!isIDValid(currTokenStr)) {
-            throw new SyntaxError("A variable name can only consist of alphanumeric characters and underscores",
+            throw new SyntaxError("Missing a variable name after the declaration keyword '" + currTokenStr + "'",
                     lexer.getCurrentLine());
         }
 
-        Token idToken = currToken;
+        SymbolTable symbolTable = SymbolTable.getInstance();
+        currTokenStr = currToken.getValue();
+        // Check if the ID is valid
+        if (symbolTable.isID(currTokenStr, scope)) {
+            throw new SyntaxError("Cannot redeclare the existing variable '" + currTokenStr + "'",
+                    lexer.getCurrentLine());
+        } else if (symbolTable.isKeyword(currTokenStr) || symbolTable.isType(currTokenStr)) {
+            throw new SyntaxError("Cannot use the reserved keyword '" + currTokenStr +
+                    "' for a variable name", lexer.getCurrentLine());
+        } else if (symbolTable.isOperator(currTokenStr)) {
+            throw new SyntaxError("Cannot use the operator '" + currTokenStr +
+                    "' as a variable name", lexer.getCurrentLine());
+        } else if (!isIDNameValid(currTokenStr)) {
+            throw new SyntaxError("The variable name '" + currTokenStr +
+                    "' can only consist of alphanumeric characters and underscores", lexer.getCurrentLine());
+        }
+
         String id = currTokenStr;
         currToken = lexer.getNextToken();
-        // Check if ';' is missing
+        // Check if ':' is missing
         if (currToken == null) {
             throw new SyntaxError("Missing ':' after '" + id + "'", lexer.getCurrentLine());
         }
 
         currTokenType = currToken.getType();
-        // Check if the token is ';'
-        if (currTokenType != TokenType.SEMICOLON) {
+        // Check if the token is ':'
+        if (currTokenType != TokenType.COLON) {
             throw new SyntaxError("Expected ':' after '" + id + "'", lexer.getCurrentLine());
         }
 
@@ -97,13 +123,13 @@ public class StatementParser {
         TypeInfo idDataType = (TypeInfo) symbolTable.getType(currTokenStr);
         // Check if the token is a valid type
         if (idDataType == null) {
-            throw new SyntaxError("Invalid type for '" + id + "'", lexer.getCurrentLine());
+            throw new SyntaxError("Invalid type for variable '" + id + "'", lexer.getCurrentLine());
         }
 
         currToken = lexer.getNextToken();
         // Check if '=' is present
         if (currToken == null) {
-            throw new SyntaxError("Missing '='", lexer.getCurrentLine());
+            throw new SyntaxError("Missing '=' after '" + currTokenStr + "'", lexer.getCurrentLine());
         }
 
         currTokenStr = currToken.getValue();
@@ -113,13 +139,20 @@ public class StatementParser {
         }
 
         Node assignmentRoot = new Node(NodeType.ASSIGNMENT);
-        Node idDeclRoot = new Node(NodeType.MUTABLE_ID_DECL);
-        TokenNode idTokenNode = new TokenNode(idToken, idDataType);
-        idDeclRoot.addChild(idTokenNode);
+        IDNode idDeclRoot = new IDNode(NodeType.MUTABLE_ID_DECL, id, true, idDataType);
         assignmentRoot.addChild(idDeclRoot);
         return assignmentRoot;
     }
 
+    /**
+     * Attempts to parse the left-hand-side of an assignment statement.
+     *
+     * @param scope scope in which the ID exists.
+     * @return an AST node that is the root of the assignment statement
+     * or null if the statement is not a proper assignment statement.
+     * @throws SyntaxError if there is a syntax error.
+     * @throws IOException if there is an IO exception.
+     */
     private Node parseLHS(Block scope) throws SyntaxError, IOException {
         /*
         var ID: type = ...;
@@ -144,19 +177,20 @@ public class StatementParser {
         }
 
         String currTokenStr = currToken.getValue();
+        String id = currTokenStr;
         SymbolTable symbolTable = SymbolTable.getInstance();
         tempTokenBuffer.addLast(currToken);
         // Check if token is an existing ID
-        if (!symbolTable.isID(currTokenStr, scope)) {
+        IDInfo idInfo = (IDInfo) symbolTable.getID(id, scope);
+        if (idInfo == null) {
             lexer.putBack(tempTokenBuffer);
             return null;
         }
 
-        Token idToken = currToken;
         currToken = lexer.getNextToken();
         // Check if '=' is present
         if (currToken == null) {
-            throw new SyntaxError("Expected an assignment or a valid expression", lexer.getCurrentLine());
+            throw new SyntaxError("Missing ';' after '" + currTokenStr + "'", lexer.getCurrentLine());
         }
 
         TokenType currTokenType = currToken.getType();
@@ -168,24 +202,91 @@ public class StatementParser {
         }
 
         assignmentRoot = new Node(NodeType.ASSIGNMENT);
-        Node idReassignmentRoot = new Node(NodeType.ID_REASSIGNMENT);
-        TokenNode idTokenNode = new TokenNode(idToken);
-        idReassignmentRoot.addChild(idTokenNode);
+        TypeInfo idDataType = idInfo.getDataType();
+        IDNode idReassignmentRoot = new IDNode(NodeType.ID_REASSIGNMENT, id, false, idDataType);
         assignmentRoot.addChild(idReassignmentRoot);
         return assignmentRoot;
     }
 
+    private Node checkLHSAndExprTypes(IDNode lhsNode, ExpressionNode exprNode) throws SyntaxError {
+        String lhsID = lhsNode.getId();
+        TypeInfo lhsDataType = lhsNode.getDataType();
+        TypeInfo exprDataType = exprNode.getDataType();
+        String lhsDataTypeId = lhsDataType.getId();
+        String exprDataTypeId = exprDataType.getId();
+        TypeTable typeTable = TypeTable.getInstance();
+
+        // Type check the left-hand side and the expression to see if they are compatible
+        boolean typeCompatible = typeTable.AreTypesCompatibleUsingBinaryOperator(
+                TokenType.ASSIGNMENT, lhsDataType, exprDataType);
+        if (!typeCompatible) {
+            throw new SyntaxError(
+                    "Variable '" + lhsID + "' of type '" + lhsDataTypeId +
+                            "' and the expression on the right of type '" + exprDataTypeId +
+                            "' are not compatible using the operator '='",
+                    lexer.getCurrentLine());
+        }
+
+        if (lhsDataType == exprDataType) {
+            return exprNode;
+        }
+
+        // If the two types are not the same but compatible with the given operator,
+        // add a conversion node that indicates the type to be converted to
+        TypeConversion typeConversion = typeTable.getTypeConversion(lhsDataType, exprDataType);
+        if (!typeConversion.isImplicit()) {
+            // If the type conversion is not implicit, there must be type casting
+            // If not, throw an exception
+            throw new SyntaxError(
+                    "Missing type casting from an expression of type '" + exprDataTypeId +
+                            "' to variable '" + lhsID + "' of type '" + lhsDataTypeId + "'",
+                    lexer.getCurrentLine());
+        }
+        TypeInfo resultType = typeConversion.getResultType();
+        DataTypeNode typeConversionNode = new DataTypeNode(NodeType.TYPE_CONVERSION, resultType);
+        typeConversionNode.addChild(exprNode);
+        return typeConversionNode;
+    }
+
+    /**
+     * Attempts to parse an assignment statement or an expression.
+     *
+     * @param scope scope of the assignment or the expression.
+     * @return an AST node that is the root of the assignment statement or an expression
+     * or null if it is empty or neither a statement nor an expression.
+     * @throws SyntaxError if there is a syntax error.
+     * @throws IOException if there is an IO exception.
+     */
     public Node parseStatement(Block scope) throws SyntaxError, IOException {
         // Assignment or pure expression
         Node assignmentRoot = parseIDDeclaration(scope);
         if (assignmentRoot == null) {
             assignmentRoot = parseLHS(scope);
         }
-        Node exprRoot = exprParser.parseExpression(scope);
+        ExpressionNode exprRoot = (ExpressionNode) exprParser.parseExpression(scope);
+
+        // Check if ';' is present
+        Token currToken = lexer.getNextToken();
+        if (currToken == null || currToken.getType() != TokenType.SEMICOLON) {
+            throw new SyntaxError("Missing ';' at the end of the statement", lexer.getCurrentLine());
+        }
+
         if (assignmentRoot == null) {
             return exprRoot;
         }
-        assignmentRoot.addChild(exprRoot);
+
+        // Type check the data returned by the expression and the left-hand side
+        IDNode lhsNode = (IDNode) assignmentRoot.getChild(0);
+        Node rhsNode = checkLHSAndExprTypes(lhsNode, exprRoot);
+
+        String id = lhsNode.getId();
+        TypeInfo idDataType = lhsNode.getDataType();
+        // Add a new ID to the symbol table if the left-hand side is an ID declaration
+        if (lhsNode.getNodeType() == NodeType.MUTABLE_ID_DECL) {
+            SymbolTable.getInstance().set(new IDInfo(id, scope, idDataType, true));
+        }
+
+        assignmentRoot.addChild(rhsNode);
         return assignmentRoot;
     }
 
